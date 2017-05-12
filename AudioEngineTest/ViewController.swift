6051d4b3a63677e37575dd212b3a77691d76e4cc
Peter Bauer <p.bauer@htl-leonding.ac.at>
2017-05-12 16:39:20 +0200

    Initial Commit

---
+++ AudioEngineTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioEngineTest
+//
+//  Created by Peter Bauer on 12/05/2017.
+//  Copyright © 2017 Peter Bauer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
